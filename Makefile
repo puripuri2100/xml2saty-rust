f8a012d802d95e2e0866b02a7731f2d051b9bc6e
puripuri2100 <puripuri2100@gmail.com>
2020-02-10 01:53:11 +0900

    update

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ build:
 test:
 	target/debug/xml2saty -f t/t.xml -o t/t.saty -c t/tconfig.json
 	cd t && satysfi t.saty
+	target/debug/xml2saty -f t/4.xml -o t/4.saty -c t/4.json
+	cd t && satysfi 4.saty
 
 testwin:
 	target\debug\xml2saty.exe -f "t/t.xml" -o "t/t.saty" -c "t/tconfig.json"
--- src/xmlparse.rs
+++ src/xmlparse.rs
@@ -10,7 +10,7 @@ use json::JsonValue;
 
 
 #[derive(Debug)]
-enum SATySFiType {
+enum SATySFiTypeTerm {
   BlockText,
   InlineText,
   SATySFiString,
@@ -20,14 +20,21 @@ enum SATySFiType {
   SATySFiFunction,
 }
 
+#[derive(Debug)]
+enum SATySFiType {
+    Normal(SATySFiTypeTerm),
+    List(SATySFiTypeTerm),
+}
+
 fn type_paren_l (config:&&JsonValue, tag:&str) -> String{
   let value = &config[tag];
   let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
   let satysfi_type = read_type(satysfi_type_str);
   match satysfi_type {
-    SATySFiType::InlineText => {"{".to_string()}
-    SATySFiType::BlockText => {"'<".to_string()}
-    SATySFiType::SATySFiString => {"```".to_string()}
+    SATySFiType::List(_) => {"[".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::InlineText) => {"{".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::BlockText) => {"'<".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::SATySFiString) => {"```".to_string()}
     _ => {"".to_string()}
   }
 }
@@ -38,30 +45,60 @@ fn type_paren_r (config:&&JsonValue, tag:&str) -> String{
   let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
   let satysfi_type = read_type(satysfi_type_str);
   match satysfi_type {
-    SATySFiType::InlineText => {"}".to_string()}
-    SATySFiType::BlockText => {">".to_string()}
-    SATySFiType::SATySFiString => {"```".to_string()}
+    SATySFiType::List(_) => {"]".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::InlineText) => {"}".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::BlockText) => {">".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::SATySFiString) => {"```".to_string()}
     _ => {"".to_string()}
   }
 }
 
 
+fn type_paren_l_lst (config:&&JsonValue, tag:&str) -> String{
+  let value = &config[tag];
+  let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
+  let satysfi_type = read_type(satysfi_type_str);
+  match satysfi_type {
+    SATySFiType::List(SATySFiTypeTerm::InlineText) => {"{".to_string()}
+    SATySFiType::List(SATySFiTypeTerm::BlockText) => {"'<".to_string()}
+    SATySFiType::List(SATySFiTypeTerm::SATySFiString) => {"```".to_string()}
+    _ => {"".to_string()}
+  }
+}
+
+
+fn type_paren_r_lst (config:&&JsonValue, tag:&str) -> String{
+  let value = &config[tag];
+  let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
+  let satysfi_type = read_type(satysfi_type_str);
+  match satysfi_type {
+    SATySFiType::List(SATySFiTypeTerm::InlineText) => {"}".to_string()}
+    SATySFiType::List(SATySFiTypeTerm::BlockText) => {">".to_string()}
+    SATySFiType::List(SATySFiTypeTerm::SATySFiString) => {"```".to_string()}
+    _ => {"".to_string()}
+  }
+}
+
 fn type_paren (t:&SATySFiType, text:&str) -> String{
   match t {
-    SATySFiType::InlineText => {format!("{{{0}}}", &text)}
-    SATySFiType::BlockText => {format!("'<{}>",&text)}
-    SATySFiType::SATySFiString => {format!("```{}```",&text)}
+    SATySFiType::List(_) => {format!("[{}]",&text)}
+    SATySFiType::Normal(SATySFiTypeTerm::InlineText) => {format!("{{{0}}}", &text)}
+    SATySFiType::Normal(SATySFiTypeTerm::BlockText) => {format!("'<{}>",&text)}
+    SATySFiType::Normal(SATySFiTypeTerm::SATySFiString) => {format!("```{}```",&text)}
     _ => {text.to_string()}
   }
 }
+
 
 fn type_semicolon (config:&&JsonValue, btag:&str) -> String{
   let value = &config[btag];
   let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
   let satysfi_type = read_type(satysfi_type_str);
   match satysfi_type {
-    SATySFiType::InlineText => {";".to_string()}
-    SATySFiType::BlockText => {";".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::InlineText) => {";".to_string()}
+    SATySFiType::Normal(SATySFiTypeTerm::BlockText) => {";".to_string()}
+    SATySFiType::List(SATySFiTypeTerm::InlineText) => {";".to_string()}
+    SATySFiType::List(SATySFiTypeTerm::BlockText) => {";".to_string()}
     _ => {"".to_string()}
   }
 }
@@ -82,17 +119,33 @@ fn get_attributes (vec:&Vec<attribute::OwnedAttribute>) -> Vec<(String, String)>
 
 fn read_type (t:&&str) -> SATySFiType {
   match t {
-    &"inline-text" => {SATySFiType::InlineText}
-    &"block-text" => {SATySFiType::BlockText}
-    &"string" => {SATySFiType::SATySFiString}
-    &"bool" => {SATySFiType::SATySFiBool}
-    &"int" => {SATySFiType::SATySFiInt}
-    &"float" => {SATySFiType::SATySFiFloat}
-    &"function" => {SATySFiType::SATySFiFunction}
-    _ => {SATySFiType::SATySFiFunction}
+    &"inline-text" => {SATySFiType::Normal(SATySFiTypeTerm::InlineText)}
+    &"block-text" => {SATySFiType::Normal(SATySFiTypeTerm::BlockText)}
+    &"string" => {SATySFiType::Normal(SATySFiTypeTerm::SATySFiString)}
+    &"bool" => {SATySFiType::Normal(SATySFiTypeTerm::SATySFiBool)}
+    &"int" => {SATySFiType::Normal(SATySFiTypeTerm::SATySFiInt)}
+    &"float" => {SATySFiType::Normal(SATySFiTypeTerm::SATySFiFloat)}
+    &"function" => {SATySFiType::Normal(SATySFiTypeTerm::SATySFiFunction)}
+    &"inline-text list" => {SATySFiType::List(SATySFiTypeTerm::InlineText)}
+    &"block-text list" => {SATySFiType::List(SATySFiTypeTerm::BlockText)}
+    &"string list" => {SATySFiType::List(SATySFiTypeTerm::SATySFiString)}
+    &"bool list" => {SATySFiType::List(SATySFiTypeTerm::SATySFiBool)}
+    &"int list" => {SATySFiType::List(SATySFiTypeTerm::SATySFiInt)}
+    &"float list" => {SATySFiType::List(SATySFiTypeTerm::SATySFiFloat)}
+    &"function list" => {SATySFiType::List(SATySFiTypeTerm::SATySFiFunction)}
+    _ => {SATySFiType::Normal(SATySFiTypeTerm::SATySFiFunction)}
   }
 }
 
+fn is_list (config:&&JsonValue, btag:&str) -> bool {
+  let value = &config[btag];
+  let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
+  let t = read_type(satysfi_type_str);
+  match t {
+    SATySFiType::List(_) => {true}
+    _ => {false}
+  }
+}
 
 fn make_attrib_string (config:&&JsonValue, name:&str, local_attribs_lst:&Vec<(String, String)>) -> String {
   let value = &config[name];
@@ -112,7 +165,7 @@ fn make_attrib_string (config:&&JsonValue, name:&str, local_attribs_lst:&Vec<(St
   let len = local_attribs_lst.len();
   for i in 0 .. len {
     let (tag,v) = &local_attribs_lst[i];
-    let (_,t,n) = &mut c.iter().filter( |(x,_,_)| x == tag).next().unwrap_or(&("",SATySFiType::SATySFiFunction,0));
+    let (_,t,n) = &mut c.iter().filter( |(x,_,_)| x == tag).next().unwrap_or(&("",SATySFiType::Normal(SATySFiTypeTerm::SATySFiFunction),0));
     let _ = if n <= &0 {(None)} else {lst.remove(n - 1)};
     let _ = if n <= &0 {()} else {lst.insert(n - 1, Some((t,v.to_string())))};
   };
@@ -136,8 +189,10 @@ fn to_cmd (config:&&JsonValue, btag:&str, name:&str) -> String {
   let satysfi_type_str = &value["type"].as_str().unwrap_or("function");
   let satysfi_type = read_type(satysfi_type_str);
   match satysfi_type {
-    SATySFiType::InlineText => {format!("\\{}",name)}
-    SATySFiType::BlockText => {format!("+{}",name)}
+    SATySFiType::Normal(SATySFiTypeTerm::InlineText) => {format!("\\{}",name)}
+    SATySFiType::Normal(SATySFiTypeTerm::BlockText) => {format!("+{}",name)}
+    SATySFiType::List(SATySFiTypeTerm::InlineText) => {format!("\\{}",name)}
+    SATySFiType::List(SATySFiTypeTerm::BlockText) => {format!("+{}",name)}
     _ => {name.to_string()}
   }
 }
@@ -177,19 +232,29 @@ pub fn xml2string (xml:BufReader<std::fs::File>, data:&JsonValue) -> String {
                 let a_s = make_attrib_string(&config_attrib_lst,&name ,&local_attribs_lst);
                 let btag = stack.iter().last().unwrap();
                 let cmd = &to_cmd(&config_attrib_lst,btag,&name);
-                xml_text.push_str(&format!("{}{}({}", cmd, a_s,type_paren_l(&config_attrib_lst,&name)));
+                let s = if is_list(&config_attrib_lst,&btag) {
+                  format!("({}{}{}({}",type_paren_l_lst(&config_attrib_lst,&btag), cmd, a_s,type_paren_l(&config_attrib_lst,&name))
+                } else {
+                  format!("{}{}({}", cmd, a_s,type_paren_l(&config_attrib_lst,&name))
+                };
+                xml_text.push_str(&s);
                 stack.push(name);
             }
             Ok(XmlEvent::EndElement { name }) => {
                 let name = format!("{}",name);
                 let _ = stack.pop();
                 let btag = stack.iter().last().unwrap();
-                xml_text.push_str(&format!("{}){}", type_paren_r(&config_attrib_lst,&name), type_semicolon(&config_attrib_lst,&btag)));
+                let s = if is_list(&config_attrib_lst,&btag) {
+                  format!("{}){}{});",type_paren_r(&config_attrib_lst,&name), type_semicolon(&config_attrib_lst,&btag),type_paren_r_lst(&config_attrib_lst,&btag))
+                } else {
+                  format!("{}){}", type_paren_r(&config_attrib_lst,&name), type_semicolon(&config_attrib_lst,&btag))
+                };
+                xml_text.push_str(&s);
             }
-            Ok(XmlEvent::CData(text)) => {
+            Ok(XmlEvent::CData(..)) => {
               ()
             }
-            Ok(XmlEvent::ProcessingInstruction {name, ..}) => {
+            Ok(XmlEvent::ProcessingInstruction {..}) => {
               ()
             }
             Ok(XmlEvent::Characters(text)) => {
